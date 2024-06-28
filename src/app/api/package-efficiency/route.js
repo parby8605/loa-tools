441e89b27ca87feab20889a295efe37134a12e9c
parby8605 <lhj971011@gmail.com>
2024-06-29 00:12:10 +0900

    #1 feat: api 요청 구현 완료

--- src/app/api/package-efficiency/route.js
+++ src/app/api/package-efficiency/route.js
@@ -1,27 +1,25 @@
-import { NextResponse } from "next/server";
+import { NextResponse } from 'next/server'
 
 export async function POST(req) {
-  const baseURL = process.env.NEXT_PUBLIC_BASE_URL || "";
-  const reqParams = await req.json();
+  const baseURL = process.env.NEXT_PUBLIC_BASE_URL || ''
+  const reqParams = await req.json()
   try {
-    console.log(process.env.NEXT_PUBLIC_API_MAIN_KEY);
     const response = await fetch(`${baseURL}/markets/items`, {
-      method: "POST",
+      method: 'POST',
       // prettier-ignore
       headers: {
             'Content-Type': 'application/json',
             'authorization': `bearer ${process.env.API_MAIN_KEY}`,
           },
       body: JSON.stringify(reqParams),
-    });
+    })
     if (!response.ok) {
-      throw new Error(`Fetch Error! status: ${response.status}`);
+      throw new Error(`Fetch Error! status: ${response.status}`)
     }
-    const data = await response.json();
-    console.log(data.Items);
-    return NextResponse.json(data.Items, { status: 200 });
+    const data = await response.json()
+    return NextResponse.json(data.Items, { status: 200 })
   } catch (e) {
-    console.log("Fetching error : ", e);
-    return NextResponse.json({ error: e.message });
+    console.log('Fetching error : ', e)
+    return NextResponse.json({ error: e.message })
   }
 }
--- src/app/components/MaterialLoader/MaterialLoader.js
+++ src/app/components/MaterialLoader/MaterialLoader.js
@@ -1,6 +1,6 @@
-"use client";
+'use client'
 
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 
 export default function MaterialLoader() {
   /**
@@ -11,70 +11,88 @@ export default function MaterialLoader() {
    * 4. 강석 종류 -> ItemName: 강석 으로 요청하면 묶여서 옴
    * 5. 추가 강화 재료 -> CategoryCode: 50020 ItemName : "태양"
    */
-  const [honorShard, setHonorShard] = useState([]);
-  const [oreha, setOreha] = useState([]);
-  const [leafStone, setLeafStone] = useState([]);
-  const [forgeStone, setForgeStone] = useState([]);
-  const [auxMaterial, setAuxMaterial] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [honorShard, setHonorShard] = useState([])
+  const [oreha, setOreha] = useState([])
+  const [leafStone, setLeafStone] = useState([])
+  const [forgeStone, setForgeStone] = useState([])
+  const [auxMaterial, setAuxMaterial] = useState([])
+  const [loading, setLoading] = useState(true)
 
   /** 재료 fetch api : reqParams에 body 작성해서 파라미터로 넘김( ex : { ItemName: "명예의 파편" }) */
   async function fetchItems(reqParams) {
     try {
-      const response = await fetch("/api/package-efficiency", {
-        method: "POST",
+      const response = await fetch('/api/package-efficiency', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify(reqParams),
-      });
+      })
 
       if (!response.ok) {
-        throw new Error(`Fetch Error! status: ${response.status}`);
+        throw new Error(`Fetch Error! status: ${response.status}`)
       }
 
-      const data = await response.json();
-      return data;
+      const data = await response.json()
+      return data
     } catch (e) {
-      console.error("Fetching error : ", e);
+      console.error('Fetching error : ', e)
     }
   }
   useEffect(() => {
     async function fetchAllMaterial() {
       const honorShard = await fetchItems({
-        Sort: "GRADE",
+        Sort: 'GRADE',
         CategoryCode: 50010,
-        ItemName: "명예의 파편 주머니(대)",
+        ItemName: '명예의 파편 주머니(대)',
         ItemTier: 3,
-        SortCondition: "ASC",
-      });
-      setHonorShard(honorShard);
+        SortCondition: 'ASC',
+      })
+      setHonorShard(honorShard)
 
       const oreha = await fetchItems({
-        Sort: "GRADE",
+        Sort: 'GRADE',
         CategoryCode: 50010,
-        ItemName: "오레하",
+        ItemName: '오레하',
         ItemTier: 3,
-        SortCondition: "ASC",
-      });
-      setOreha(oreha);
+        SortCondition: 'ASC',
+      })
+      setOreha(oreha)
 
       const leafStone = fetchItems({
-        Sort: "GRADE",
+        Sort: 'GRADE',
         CategoryCode: 50010,
-        ItemName: "돌파석",
+        ItemName: '돌파석',
         ItemTier: 3,
-        SortCondition: "ASC",
-      });
-      setLeafStone(leafStone);
+        SortCondition: 'ASC',
+      })
+      setLeafStone(leafStone)
+
+      const forgeStone = fetchItems({
+        Sort: 'GRADE',
+        CategoryCode: 50010,
+        ItemName: '강석',
+        ItemTier: 3,
+        SortCondition: 'ASC',
+      })
+      setForgeStone(forgeStone)
+
+      const auxMaterial = fetchItems({
+        Sort: 'GRADE',
+        CategoryCode: 50020,
+        ItemName: '태양',
+        ItemTier: 3,
+        SortCondition: 'ASC',
+      })
+      setAuxMaterial(auxMaterial)
     }
-    fetchAllMaterial();
-    setLoading(false);
-  }, []);
+    fetchAllMaterial()
+    setLoading(false)
+  }, [])
 
   // 데이터 로딩중일경우 노출
   if (loading) {
-    return <div>Now Loading...</div>;
+    return <div>Now Loading...</div>
   }
 
   return (
@@ -85,5 +103,5 @@ export default function MaterialLoader() {
         <div>oreha : {oreha[0]?.Grade}</div>
       </div>
     </>
-  );
+  )
 }
