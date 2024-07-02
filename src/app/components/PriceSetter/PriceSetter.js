ebddda2d0cc5667267327db7ae3a3ad1bb5beeaf
HeeJeong <lhj971011@gmail.com>
2024-07-02 18:46:22 +0900

    #1 design: 레이아웃 조정

--- src/app/components/PriceSetter/PriceSetter.js
+++ src/app/components/PriceSetter/PriceSetter.js
@@ -18,21 +18,36 @@ export default function PriceSetter() {
       </div>
       <div className={style.priceWrapper}>
         <div className={style.price}>
-          <div>상품권 할인율</div>
-          <input onChange={() => setDiscount} />
+          <div className='flex'>상품권 할인율</div>
+          <input
+            className={style.nospinner}
+            onChange={() => setDiscount}
+            type='number'
+            defaultValue={0}
+          />
         </div>
         <div className={style.price}>
           <div>블루 크리스탈 가격 입력</div>
           <div className={style.priceRatio}>
             <div className='pr-4'>2700 : </div>
-            <input onChange={() => setBCPrice} />
+            <input
+              className={style.nospinner}
+              onChange={() => setBCPrice}
+              type='number'
+              defaultValue={0}
+            />
           </div>
         </div>
         <div className={style.price}>
           <div>유저간 골드 거래가</div>
           <div className={style.priceRatio}>
             <div className='pr-4'>100 : </div>
-            <input onChange={() => setUserGoldPrice} />
+            <input
+              className={style.nospinner}
+              onChange={() => setUserGoldPrice}
+              type='number'
+              defaultValue={0}
+            />
           </div>
         </div>
       </div>
