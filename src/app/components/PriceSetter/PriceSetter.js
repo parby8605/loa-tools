3688fa47d001626915ef4697e807d0a90f59d1c0
parby8605 <lhj971011@gmail.com>
2024-07-02 18:28:05 +0900

    #1 design: 가격설정 컴포넌트 작성"

--- src/app/components/PriceSetter/PriceSetter.js
+++ src/app/components/PriceSetter/PriceSetter.js
@@ -1,31 +1,39 @@
 'use client'
 
 import style from '@/app/components/PriceSetter/PriceSetter.module.css'
+import Image from 'next/image'
 import { useState } from 'react'
 
 export default function PriceSetter() {
   const [discount, setDiscount] = useState()
   const [userGoldPrice, setUserGoldPrice] = useState()
   const [BCPrice, setBCPrice] = useState()
+  const goldIMG = '/Gold.png'
 
   return (
-    <div className={style.contentWrapper}>
-      <div>골드 시세 입력</div>
+    <div className={style.priceContainer}>
+      <div className='flex flex-row items-center gap-x-4 mb-4'>
+        <Image src={goldIMG} width={46} height={46} alt='GOLD' />
+        <div>골드 시세 입력</div>
+      </div>
       <div className={style.priceWrapper}>
-        <div>
-          <div>할인율</div>
+        <div className={style.price}>
+          <div>상품권 할인율</div>
           <input onChange={() => setDiscount} />
         </div>
-        <div>
+        <div className={style.price}>
           <div>블루 크리스탈 가격 입력</div>
-          <div>
-            <div>2700 : </div>
+          <div className={style.priceRatio}>
+            <div className='pr-4'>2700 : </div>
             <input onChange={() => setBCPrice} />
           </div>
         </div>
-        <div>
+        <div className={style.price}>
           <div>유저간 골드 거래가</div>
-          <input onChange={() => setUserGoldPrice} />
+          <div className={style.priceRatio}>
+            <div className='pr-4'>100 : </div>
+            <input onChange={() => setUserGoldPrice} />
+          </div>
         </div>
       </div>
     </div>
--- src/app/package-efficiency/page.js
+++ src/app/package-efficiency/page.js
@@ -3,13 +3,8 @@ import MaterialLoader from '@/app/components/MaterialLoader/MaterialLoader'
 export default function packageEfficiency() {
   return (
     <>
-      <div className='flex flex-row flex-grow gap-5 m-5'>
-        <div className='w-[20%]'>
-          <MaterialLoader />
-        </div>
-        <div>
-          <div>여기에 패키지 선택 컴포넌트</div>
-        </div>
+      <div className='flex'>
+        <MaterialLoader />
       </div>
     </>
   )
